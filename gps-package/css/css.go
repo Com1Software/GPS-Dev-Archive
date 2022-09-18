3eafa6358cc28c732e97c2ead6285213b8d76d3e
Lenovo 15 <information@com1software.com>
2022-09-18 14:17:32 -0400

    added new code

---
+++ gps-package/css/css.go
@@ -0,0 +1,43 @@
+package css
+
+//	"fmt"
+
+//
+//------------------ (c) 1992-2022 Com1 Software Development
+//
+
+// BaseStyle : This is a base style sheet
+func BaseStyle(xdata string) string {
+	xdata = xdata + "<style>"
+	xdata = xdata + "body {"
+	xdata = xdata + "    background-color: lightblue;"
+	xdata = xdata + "}"
+	xdata = xdata + "	h1 {"
+	xdata = xdata + "	color: white;"
+	xdata = xdata + "	text-align: center;"
+	xdata = xdata + "}"
+	xdata = xdata + "	p {"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "}"
+	xdata = xdata + "</style>"
+	return xdata
+}
+
+// Test1Style : Setting style sheet
+func Test1Style(xdata string) string {
+	xdata = xdata + "<style>"
+	xdata = xdata + "body {"
+	xdata = xdata + "    background-color: white;"
+	xdata = xdata + "}"
+	xdata = xdata + "	h1 {"
+	xdata = xdata + "	color: white;"
+	xdata = xdata + "	text-align: center;"
+	xdata = xdata + "}"
+	xdata = xdata + "	p {"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "}"
+	xdata = xdata + "</style>"
+	return xdata
+}
