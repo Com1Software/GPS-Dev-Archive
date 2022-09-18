22a3e34b41f77a325023090da277a11d1b83c2c1
Lenovo 15 <information@com1software.com>
2022-09-18 14:18:19 -0400

    added new code

---
+++ gps-package/gps.go
@@ -0,0 +1,41 @@
+package playground
+
+import (
+	"gps-package/css"
+	"gps-package/js"
+)
+
+// Start Page :
+func StartPage() string {
+	xdata := "<!DOCTYPE html>"
+	xdata = xdata + "<html>"
+	xdata = xdata + "<head>"
+	xdata = xdata + "Start Page</title>"
+	xdata = css.SettingsStyle(xdata)
+	xdata = js.DateTimeDisplay(xdata)
+	xdata = xdata + "</head>"
+	xdata = xdata + "<body onload='startTime()'>"
+	xdata = xdata + "<p>XStart Page</p>"
+	xdata = xdata + "<BR> GPS-Dashboard <BR> (c) 1992-2022 Com1 Software Development"
+	xdata = xdata + " </body>"
+	xdata = xdata + " </html>"
+	return xdata
+}
+
+// TestPage1 :
+func TestPage1() string {
+	xdata := "<!DOCTYPE html>"
+	xdata = xdata + "<html>"
+	xdata = xdata + "<head>"
+	xdata = xdata + "<title>TestPage1</title>"
+	xdata = css.SettingsStyle(xdata)
+	xdata = js.DateTimeDisplay(xdata)
+	xdata = xdata + "</head>"
+	xdata = xdata + "<body onload='startTime()'>"
+	xdata = xdata + "<p>Test Page 1</p>"
+	xdata = xdata + "<BR> Test page 1 <BR> (c) 1992-2022 Com1 Software Development"
+	xdata = xdata + " </body>"
+	xdata = xdata + " </html>"
+	return xdata
+}
+Footer
