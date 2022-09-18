9d85d3be788a89508ab17f3e55b86a2e3951543f
Lenovo 15 <information@com1software.com>
2022-09-18 14:57:24 -0400

    updates

--- main.go
+++ main.go
@@ -3,15 +3,272 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
+	"runtime"
 	"strings"
 
 	"go.bug.st/serial"
 )
 
+func getGPSPosition(sentence string) (string, string, string, string, string, string, string) {
+	data := strings.Split(sentence, ",")
+	id := ""
+	latitude := ""
+	longitude := ""
+	ns := ""
+	ew := ""
+	speed := ""
+	degree := ""
+
+	switch {
+	case string(data[0]) == "$GPGGA":
+		// id = data[0]
+		latitude = data[2]
+		ns = data[3]
+		longitude = data[4]
+		ew = data[5]
+	case string(data[0]) == "$GPGLL":
+		// id = data[0]
+		latitude = data[1]
+		ns = data[2]
+		longitude = data[3]
+		ew = data[4]
+
+	case string(data[0]) == "$GPVTG":
+		//	id = data[0]
+		degree = data[1]
+
+	case string(data[0]) == "$GPRMC":
+		id = data[0]
+		latitude = data[3]
+		ns = data[4]
+		longitude = data[5]
+		ew = data[6]
+		speed = data[7]
+		degree = data[8]
+
+	case string(data[0]) == "$GPGSA":
+
+	case string(data[0]) == "$GPGSV":
+
+	case string(data[0]) == "$GPTXT":
+
+	default:
+		fmt.Println("-- %s", data[0])
+
+	}
+
+	return id, latitude, longitude, ns, ew, speed, degree
+}
+
+// Openbrowser : Opens default web browser to specified url
+func Openbrowser(url string) error {
+	var cmd string
+	var args []string
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
+
+//------------------------------------------------------------------  Pages
+func StartPage() string {
+	xdata := "<!DOCTYPE html>"
+	xdata = xdata + "<html>"
+	xdata = xdata + "<head>"
+	xdata = xdata + "Start Page</title>"
+	xdata = SettingsStyle(xdata)
+	xdata = DateTimeDisplay(xdata)
+	xdata = xdata + "</head>"
+	xdata = xdata + "<body onload='startTime()'>"
+	xdata = xdata + "<p>XStart Page</p>"
+	xdata = xdata + "<BR> GPS-Dashboard <BR> (c) 1992-2022 Com1 Software Development"
+	xdata = xdata + " </body>"
+	xdata = xdata + " </html>"
+	return xdata
+}
+
+// TestPage1 :
+func TestPage1() string {
+	xdata := "<!DOCTYPE html>"
+	xdata = xdata + "<html>"
+	xdata = xdata + "<head>"
+	xdata = xdata + "<title>TestPage1</title>"
+	xdata = SettingsStyle(xdata)
+	xdata = DateTimeDisplay(xdata)
+	xdata = xdata + "</head>"
+	xdata = xdata + "<body onload='startTime()'>"
+	xdata = xdata + "<p>Test Page 1</p>"
+	xdata = xdata + "<BR> Test page 1 <BR> (c) 1992-2022 Com1 Software Development"
+	xdata = xdata + " </body>"
+	xdata = xdata + " </html>"
+	return xdata
+}
+
+//----------------------------------------------------------------------- JS
+func DateTimeDisplay(xdata string) string {
+	//------------------------------------------------------------------------
+	xdata = xdata + "<script>"
+	xdata = xdata + "function startTime() {"
+	xdata = xdata + "  var today = new Date();"
+	xdata = xdata + "  var d = today.getDay();"
+	xdata = xdata + "  var h = today.getHours();"
+	xdata = xdata + "  var m = today.getMinutes();"
+	xdata = xdata + "  var s = today.getSeconds();"
+	xdata = xdata + "  var ampm = h >= 12 ? 'pm' : 'am';"
+	xdata = xdata + "  var mo = today.getMonth();"
+	xdata = xdata + "  var dm = today.getDate();"
+	xdata = xdata + "  var yr = today.getFullYear();"
+	xdata = xdata + "  m = checkTimeMS(m);"
+	xdata = xdata + "  s = checkTimeMS(s);"
+	xdata = xdata + "  h = checkTimeH(h);"
+	//------------------------------------------------------------------------
+	xdata = xdata + "  switch (d) {"
+	xdata = xdata + "    case 0:"
+	xdata = xdata + "       day = 'Sunday';"
+	xdata = xdata + "    break;"
+	xdata = xdata + "    case 1:"
+	xdata = xdata + "    day = 'Monday';"
+	xdata = xdata + "        break;"
+	xdata = xdata + "    case 2:"
+	xdata = xdata + "        day = 'Tuesday';"
+	xdata = xdata + "        break;"
+	xdata = xdata + "    case 3:"
+	xdata = xdata + "        day = 'Wednesday';"
+	xdata = xdata + "        break;"
+	xdata = xdata + "    case 4:"
+	xdata = xdata + "        day = 'Thursday';"
+	xdata = xdata + "        break;"
+	xdata = xdata + "    case 5:"
+	xdata = xdata + "        day = 'Friday';"
+	xdata = xdata + "        break;"
+	xdata = xdata + "    case 6:"
+	xdata = xdata + "        day = 'Saturday';"
+	xdata = xdata + "}"
+	//------------------------------------------------------------------------------------
+	xdata = xdata + "  switch (mo) {"
+	xdata = xdata + "    case 0:"
+	xdata = xdata + "       month = 'January';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 1:"
+	xdata = xdata + "       month = 'Febuary';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 2:"
+	xdata = xdata + "       month = 'March';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 3:"
+	xdata = xdata + "       month = 'April';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 4:"
+	xdata = xdata + "       month = 'May';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 5:"
+	xdata = xdata + "       month = 'June';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 6:"
+	xdata = xdata + "       month = 'July';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 7:"
+	xdata = xdata + "       month = 'August';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 8:"
+	xdata = xdata + "       month = 'September';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 9:"
+	xdata = xdata + "       month = 'October';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 10:"
+	xdata = xdata + "       month = 'November';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "    case 11:"
+	xdata = xdata + "       month = 'December';"
+	xdata = xdata + "       break;"
+	xdata = xdata + "}"
+	//  -------------------------------------------------------------------
+	xdata = xdata + "  document.getElementById('txt').innerHTML = ' - '+h + ':' + m + ':' + s+' '+ampm+' - '+day+', '+month+' '+dm+', '+yr;"
+	xdata = xdata + "  var t = setTimeout(startTime, 500);"
+	xdata = xdata + "}"
+	//----------
+	xdata = xdata + "function checkTimeMS(i) {"
+	xdata = xdata + "  if (i < 10) {i = '0' + i};"
+	xdata = xdata + "  return i;"
+	xdata = xdata + "}"
+	//----------
+	xdata = xdata + "function checkTimeH(i) {"
+	xdata = xdata + "  if (i > 12) {i = i -12};"
+	xdata = xdata + "  return i;"
+	xdata = xdata + "}"
+	xdata = xdata + "</script>"
+	return xdata
+
+}
+
+//----------------------------------------------------------------------------
+// BaseStyle : This is a base style sheet
+func BaseStyle(xdata string) string {
+	xdata = xdata + "<style>"
+	xdata = xdata + "body {"
+	xdata = xdata + "    background-color: lightblue;"
+	xdata = xdata + "}"
+	xdata = xdata + "	h1 {"
+	xdata = xdata + "	color: white;"
+	xdata = xdata + "	text-align: center;"
+	xdata = xdata + "}"
+	xdata = xdata + "	p {"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "}"
+	xdata = xdata + "</style>"
+	return xdata
+}
+
+// Test1Style : Setting style sheet
+func Test1Style(xdata string) string {
+	xdata = xdata + "<style>"
+	xdata = xdata + "body {"
+	xdata = xdata + "    background-color: white;"
+	xdata = xdata + "}"
+	xdata = xdata + "	h1 {"
+	xdata = xdata + "	color: white;"
+	xdata = xdata + "	text-align: center;"
+	xdata = xdata + "}"
+	xdata = xdata + "	p {"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "}"
+	xdata = xdata + "</style>"
+	return xdata
+}
+
+func SettingsStyle(xdata string) string {
+	xdata = xdata + "<style>"
+	xdata = xdata + "body {"
+	xdata = xdata + "    background-color: white;"
+	xdata = xdata + "}"
+	xdata = xdata + "	h1 {"
+	xdata = xdata + "	color: white;"
+	xdata = xdata + "	text-align: center;"
+	xdata = xdata + "}"
+	xdata = xdata + "	p {"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "}"
+	xdata = xdata + "</style>"
+	return xdata
+}
+
+//-------------------------------------------------------------------------
 func main() {
 	fmt.Println("GPS-Track-Recorder")
-
-	// Retrieve the port list
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+	//Openbrowser("http://localhost:8080/")
 	ports, err := serial.GetPortsList()
 	if err != nil {
 		log.Fatal(err)
@@ -27,7 +284,7 @@ func main() {
 
 	// Open the first serial port detected at 9600bps N81
 	mode := &serial.Mode{
-		BaudRate: 9600,
+		BaudRate: 4800,
 		Parity:   serial.NoParity,
 		DataBits: 8,
 		StopBits: serial.OneStopBit,
@@ -37,11 +294,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//	line := ""
+	line := ""
 	buff := make([]byte, 1)
 	on := true
-	//n := 0
 	for on != false {
+		line = ""
 		for {
 			n, err := port.Read(buff)
 			if err != nil {
@@ -51,14 +308,16 @@ func main() {
 				fmt.Println("\nEOF")
 				break
 			}
-			fmt.Println(buff[:n])
-			// fmt.Printf("%s", string(buff[:n]))
-
-			// If we receive a newline stop reading
+			line = line + string(buff[:n])
 			if strings.Contains(string(buff[:n]), "\n") {
 				break
-				fmt.Println("test")
 			}
+
+		}
+		//fmt.Println(line)
+		id, latitude, longitude, ns, ew, speed, degree := getGPSPosition(line)
+		if len(id) > 0 {
+			fmt.Printf("%s  latitude=%s  %s  longitude=%s %s  knots=%s degrees=%s\n", id, latitude, ns, longitude, ew, speed, degree)
 		}
 	}
 }
