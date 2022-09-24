c8a194c78a4d54f1c4066e09ec4ee59d808f9ea3
Lenovo 15 <information@com1software.com>
2022-09-24 14:38:51 -0400

    update

--- main.go
+++ main.go
@@ -3,15 +3,82 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
 	"net/http"
-	"os"
 	"os/exec"
 	"runtime"
 	"strings"
 
 	"go.bug.st/serial"
 )
+
+//-------------------------------------------------------------------------
+func main() {
+	agent := SSE()
+	fmt.Println("GPS-Track-Recorder")
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+
+	// Print the list of detected ports
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+
+	// Open the first serial port detected at 9600bps N81
+	mode := &serial.Mode{
+		BaudRate: 4800,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	port, err := serial.Open(ports[0], mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			line := ""
+			buff := make([]byte, 1)
+			on := true
+			for on != false {
+				line = ""
+				for {
+					n, err := port.Read(buff)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if n == 0 {
+						fmt.Println("\nEOF")
+						break
+					}
+					line = line + string(buff[:n])
+					if strings.Contains(string(buff[:n]), "\n") {
+						break
+					}
+
+				}
+				id, latitude, longitude, ns, ew, speed, degree := getGPSPosition(line)
+				if len(id) > 0 {
+					// fmt.Println(line)
+					event := fmt.Sprintf("%s  latitude=%s  %s  longitude=%s %s  knots=%s degrees=%s\n", id, latitude, ns, longitude, ew, speed, degree)
+					fmt.Println(event)
+					agent.Notifier <- []byte(event)
+				}
+			}
+		}
+	}()
+
+	Openbrowser("http://localhost:8080")
+	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:8080", agent))
+
+}
 
 func getGPSPosition(sentence string) (string, string, string, string, string, string, string) {
 	data := strings.Split(sentence, ",")
@@ -88,283 +155,63 @@ func Openbrowser(url string) error {
 	return exec.Command(cmd, args...).Start()
 }
 
-//------------------------------------------------------------------  Pages
-func StartPage() string {
-	xip := "localhost"
-	xdata := "<!DOCTYPE html>"
-	xdata = xdata + "<html>"
-	xdata = xdata + "<head>"
-	xdata = xdata + "Start Page</title>"
-	xdata = SettingsStyle(xdata)
-	xdata = DateTimeDisplay(xdata)
-	xdata = xdata + "</head>"
-	xdata = xdata + "<body onload='startTime()'>"
-	xdata = xdata + "<p>XStart Page</p>"
-	host, _ := os.Hostname()
-	addrs, _ := net.LookupIP(host)
+type Agent struct {
+	Notifier    chan []byte
+	newuser     chan chan []byte
+	closinguser chan chan []byte
+	user        map[chan []byte]bool
+}
 
-	for _, addr := range addrs {
-		if ipv4 := addr.To4(); ipv4 != nil {
-			xip = fmt.Sprintf("%s", ipv4)
+func SSE() (agent *Agent) {
+	agent = &Agent{
+		Notifier:    make(chan []byte, 1),
+		newuser:     make(chan chan []byte),
+		closinguser: make(chan chan []byte),
+		user:        make(map[chan []byte]bool),
+	}
+	go agent.listen()
+	return
+}
+
+func (agent *Agent) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	flusher, ok := rw.(http.Flusher)
+	if !ok {
+		http.Error(rw, "Error ", http.StatusInternalServerError)
+		return
+	}
+	rw.Header().Set("Content-Type", "text/event-stream")
+	rw.Header().Set("Cache-Control", "no-cache")
+	rw.Header().Set("Connection", "keep-alive")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	mChan := make(chan []byte)
+	agent.newuser <- mChan
+	defer func() {
+		agent.closinguser <- mChan
+	}()
+	notify := req.Context().Done()
+	go func() {
+		<-notify
+		agent.closinguser <- mChan
+	}()
+	for {
+		fmt.Fprintf(rw, "%s", <-mChan)
+		flusher.Flush()
+	}
+
+}
+
+func (agent *Agent) listen() {
+	for {
+		select {
+		case s := <-agent.newuser:
+			agent.user[s] = true
+		case s := <-agent.closinguser:
+			delete(agent.user, s)
+		case event := <-agent.Notifier:
+			for userMChan, _ := range agent.user {
+				userMChan <- event
+			}
 		}
 	}
-	xdata = xdata + "<p> Host Port IP : " + xip + "</p>"
 
-	xdata = xdata + "  <A HREF='http://com1software.com'> [ Com1Software Home Page ] </A>  "
-	xdata = xdata + "  <A HREF='http://" + xip + ":8080/about'> [ About ] </A>  "
-	xdata = xdata + "  <A HREF='http://" + xip + ":8080/systems'> [ Systems ] </A>  "
-	xdata = xdata + "  <A HREF='http://" + xip + ":8080/playground'> [ PlayGround ] </A>  "
-	xdata = xdata + "<BR> GPS-Dashboard <BR> (c) 1992-2022 Com1 Software Development"
-	xdata = xdata + "<div id='txt'></div>"
-	xdata = xdata + " </body>"
-	xdata = xdata + " </html>"
-	return xdata
-}
-
-// TestPage1 :
-func TestPage1() string {
-	xdata := "<!DOCTYPE html>"
-	xdata = xdata + "<html>"
-	xdata = xdata + "<head>"
-	xdata = xdata + "<title>TestPage1</title>"
-	xdata = SettingsStyle(xdata)
-	xdata = DateTimeDisplay(xdata)
-	xdata = xdata + "</head>"
-	xdata = xdata + "<body onload='startTime()'>"
-	xdata = xdata + "<p>Test Page 1</p>"
-	xdata = xdata + "<BR> Test page 1 <BR> (c) 1992-2022 Com1 Software Development"
-	xdata = xdata + " </body>"
-	xdata = xdata + " </html>"
-	return xdata
-}
-
-//----------------------------------------------------------------------- JS
-func DateTimeDisplay(xdata string) string {
-	//------------------------------------------------------------------------
-	xdata = xdata + "<script>"
-	xdata = xdata + "function startTime() {"
-	xdata = xdata + "  var today = new Date();"
-	xdata = xdata + "  var d = today.getDay();"
-	xdata = xdata + "  var h = today.getHours();"
-	xdata = xdata + "  var m = today.getMinutes();"
-	xdata = xdata + "  var s = today.getSeconds();"
-	xdata = xdata + "  var ampm = h >= 12 ? 'pm' : 'am';"
-	xdata = xdata + "  var mo = today.getMonth();"
-	xdata = xdata + "  var dm = today.getDate();"
-	xdata = xdata + "  var yr = today.getFullYear();"
-	xdata = xdata + "  m = checkTimeMS(m);"
-	xdata = xdata + "  s = checkTimeMS(s);"
-	xdata = xdata + "  h = checkTimeH(h);"
-	//------------------------------------------------------------------------
-	xdata = xdata + "  switch (d) {"
-	xdata = xdata + "    case 0:"
-	xdata = xdata + "       day = 'Sunday';"
-	xdata = xdata + "    break;"
-	xdata = xdata + "    case 1:"
-	xdata = xdata + "    day = 'Monday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 2:"
-	xdata = xdata + "        day = 'Tuesday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 3:"
-	xdata = xdata + "        day = 'Wednesday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 4:"
-	xdata = xdata + "        day = 'Thursday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 5:"
-	xdata = xdata + "        day = 'Friday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 6:"
-	xdata = xdata + "        day = 'Saturday';"
-	xdata = xdata + "}"
-	//------------------------------------------------------------------------------------
-	xdata = xdata + "  switch (mo) {"
-	xdata = xdata + "    case 0:"
-	xdata = xdata + "       month = 'January';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 1:"
-	xdata = xdata + "       month = 'Febuary';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 2:"
-	xdata = xdata + "       month = 'March';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 3:"
-	xdata = xdata + "       month = 'April';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 4:"
-	xdata = xdata + "       month = 'May';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 5:"
-	xdata = xdata + "       month = 'June';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 6:"
-	xdata = xdata + "       month = 'July';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 7:"
-	xdata = xdata + "       month = 'August';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 8:"
-	xdata = xdata + "       month = 'September';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 9:"
-	xdata = xdata + "       month = 'October';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 10:"
-	xdata = xdata + "       month = 'November';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 11:"
-	xdata = xdata + "       month = 'December';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "}"
-	//  -------------------------------------------------------------------
-	xdata = xdata + "  document.getElementById('txt').innerHTML = ' - '+h + ':' + m + ':' + s+' '+ampm+' - '+day+', '+month+' '+dm+', '+yr;"
-	xdata = xdata + "  var t = setTimeout(startTime, 500);"
-	xdata = xdata + "}"
-	//----------
-	xdata = xdata + "function checkTimeMS(i) {"
-	xdata = xdata + "  if (i < 10) {i = '0' + i};"
-	xdata = xdata + "  return i;"
-	xdata = xdata + "}"
-	//----------
-	xdata = xdata + "function checkTimeH(i) {"
-	xdata = xdata + "  if (i > 12) {i = i -12};"
-	xdata = xdata + "  return i;"
-	xdata = xdata + "}"
-	xdata = xdata + "</script>"
-	return xdata
-
-}
-
-//----------------------------------------------------------------------------
-// BaseStyle : This is a base style sheet
-func BaseStyle(xdata string) string {
-	xdata = xdata + "<style>"
-	xdata = xdata + "body {"
-	xdata = xdata + "    background-color: lightblue;"
-	xdata = xdata + "}"
-	xdata = xdata + "	h1 {"
-	xdata = xdata + "	color: white;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p {"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "</style>"
-	return xdata
-}
-
-// Test1Style : Setting style sheet
-func Test1Style(xdata string) string {
-	xdata = xdata + "<style>"
-	xdata = xdata + "body {"
-	xdata = xdata + "    background-color: white;"
-	xdata = xdata + "}"
-	xdata = xdata + "	h1 {"
-	xdata = xdata + "	color: white;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p {"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "</style>"
-	return xdata
-}
-
-func SettingsStyle(xdata string) string {
-	xdata = xdata + "<style>"
-	xdata = xdata + "body {"
-	xdata = xdata + "    background-color: white;"
-	xdata = xdata + "}"
-	xdata = xdata + "	h1 {"
-	xdata = xdata + "	color: white;"
-	xdata = xdata + "	text-align: center;"
-	xdata = xdata + "}"
-	xdata = xdata + "	p {"
-	xdata = xdata + "font-family: verdana;"
-	xdata = xdata + "	font-size: 20px;"
-	xdata = xdata + "}"
-	xdata = xdata + "</style>"
-	return xdata
-}
-
-//-------------------------------------------------------------------------
-func main() {
-	GPSChan := make(chan []GPSData)
-	fmt.Println("GPS-Track-Recorder")
-	fmt.Printf("Operating System : %s\n", runtime.GOOS)
-	//------------------------------------------------ Home Page
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		xdata := StartPage()
-		fmt.Fprint(w, xdata)
-	})
-	//------------------------------------------------ Settings Page
-	http.HandleFunc("/settings", func(w http.ResponseWriter, r *http.Request) {
-		xdata := TestPage1()
-		fmt.Fprint(w, xdata)
-	})
-	go rungps(GPSChan)
-	Openbrowser("http://localhost:8080/")
-	http.ListenAndServe(":8080", nil)
-
-}
-
-type GPSData struct {
-	Line string
-}
-
-func rungps(dataChan chan []GPSData) {
-	ports, err := serial.GetPortsList()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(ports) == 0 {
-		log.Fatal("No serial ports found!")
-	}
-
-	// Print the list of detected ports
-	for _, port := range ports {
-		fmt.Printf("Found port: %v\n", port)
-	}
-
-	// Open the first serial port detected at 9600bps N81
-	mode := &serial.Mode{
-		BaudRate: 4800,
-		Parity:   serial.NoParity,
-		DataBits: 8,
-		StopBits: serial.OneStopBit,
-	}
-	port, err := serial.Open(ports[0], mode)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	line := ""
-	buff := make([]byte, 1)
-	on := true
-	for on != false {
-		line = ""
-		for {
-			n, err := port.Read(buff)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if n == 0 {
-				fmt.Println("\nEOF")
-				break
-			}
-			line = line + string(buff[:n])
-			if strings.Contains(string(buff[:n]), "\n") {
-				break
-			}
-
-		}
-		id, latitude, longitude, ns, ew, speed, degree := getGPSPosition(line)
-		if len(id) > 0 {
-			// fmt.Println(line)
-			fmt.Printf("%s  latitude=%s  %s  longitude=%s %s  knots=%s degrees=%s\n", id, latitude, ns, longitude, ew, speed, degree)
-		}
-	}
 }
